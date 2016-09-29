8aca3d07f8303d13bea758785962c1538be39556
Aswa Paul <aswapaul@gmail.com>
2016-09-29 15:19:28 +0300

    Add fieldsets and fab button

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from todo.models import *
+from django.contrib import admin
+
+class CardAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Card Data', {'fields': ['card_name', 'description']}),
+		('Todo Items', {'fields': ['todo_item']}),
+	]
+
 
 admin.site.register(TodoList)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
 admin.site.register(TodoItem)

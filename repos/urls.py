9ee03ea335b438cf1005a2295360310456e27bad
Aswa Paul <aswapaul@gmail.com>
2016-10-02 02:56:52 +0300

    Switch home page to reg/home

--- repos/urls.py
+++ repos/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views
 from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    url(r'^$', lambda u: HttpResponseRedirect('/repo')),
+    url(r'^$', lambda u: HttpResponseRedirect('/reg')),
 	url(r'^controller/logout/$', views.logout, {'next_page': '/repo/home/'}),
     url(r'^controller/', admin.site.urls),
     url(r'^repo/', include('repo.urls')),

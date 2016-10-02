d55990c7d225a45de2999abaacf7bccb497f9d59
Aswa Paul <aswapaul@gmail.com>
2016-10-02 04:32:59 +0300

    Add whitenoise

--- repos/settings.py
+++ repos/settings.py
@@ -1,8 +1,9 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -13,7 +14,7 @@ SECRET_KEY = '^tnn_n3=x2!mo2kz%73^!*lw#n!1!zx*o5d#%pee1r(n4nejth'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,8 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 LOGIN_URL = 'reg:login'
 
@@ -140,3 +146,7 @@ NORECAPTCHA_SITE_KEY = '6Lf6ricTAAAAAC_fbDp4xAiQm_TwmrxkFdtETr2j'
 NORECAPTCHA_SECRET_KEY = '6Lf6ricTAAAAAN2MsyOvRq_jMCJBeOy-jXpmXIGG'
 
 SESSION_COOKIE_AGE = 604800
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

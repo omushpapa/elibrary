e1cee04ae3b8bee4f58ef9010c2789941fd9346f
Aswa Paul <aswapaul@gmail.com>
2016-10-03 20:55:13 +0300

    Remove HEAD-added line

--- repos/settings.py
+++ repos/settings.py
@@ -4,10 +4,10 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
-=======
->>>>>>> 815575b6c1388f29002bf7d4543e01ae37e1bff9
+#=======
+#>>>>>>> 815575b6c1388f29002bf7d4543e01ae37e1bff9
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/

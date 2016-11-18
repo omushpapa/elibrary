9cafb482985b997df38fcbabd0a2c0967ce28095
Paul Aswa <aswapaul@gmail.com>
2016-11-18 14:25:34 +0300

    Add mdl design to login fields

--- repo/forms.py
+++ repo/forms.py
@@ -3,10 +3,13 @@ from django import forms
 from repo.models import UserData
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = UserData
         fields = ['username', 'password', 'email', 'user_level']
+        widgets = {
+        	'username': forms.TextInput(attrs={'class': 'mdl-textfield__input'}),
+        	'password': forms.PasswordInput(attrs={'class': 'mdl-textfield__input'})
+        }
 
 class UserEditForm(forms.ModelForm):
     class Meta:
